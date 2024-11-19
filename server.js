0854a4f1530f748f654729ce22c0ee4ce5c3f13f
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2024-11-19 19:09:35 +0000

    aula 2

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({message: 'minha api test'})
+    res.json({message: 'minha api test galdino'})
 })
 
 const port = 3000
-console.log(process.env.GEMINI_API_KEY)
 app.listen(port, () => {
     console.log(`Servidor http://localhost:${port}`)
 })
