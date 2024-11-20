55b5553db624e74d707e438fc10cc302a35959b1
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2024-11-20 19:51:29 +0000

    aula 2

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 import express from 'express'
 const app = express()
 
+const posts = [
+    {
+      id: 1,
+      descricao: "Uma foto teste",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      id: 2,
+      descricao: "Gato fazendo yoga",
+      imagem: "https://placekitten.com/400/300"
+    },
+    {
+      id: 3,
+      descricao: "Gatinho dormindo",
+      imagem: "https://placekitten.com/200/200"
+    }
+  ];
+
 app.get('/', (req, res) => {
     res.json({message: 'minha api test galdino'})
+})
+
+app.get('/posts', (req, res) => {
+    res.json(posts)
+})
+
+app.get('/posts/:id', (req, res) => {
+    const post = posts.find(post => post.id === Number(req.params.id))
+    res.json(post)
 })
 
 const port = 3000
